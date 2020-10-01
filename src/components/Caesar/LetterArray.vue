3904240c9570941eedfdb402b52c262306fe4979
Link Li <link@langchao.org>
2020-10-01 01:54:09 -0400

    Slightly improve the caesar table’s rendering performance

--- src/components/Caesar/LetterArray.vue
+++ src/components/Caesar/LetterArray.vue
@@ -3,19 +3,19 @@
     <v-stage :config="configKonva">
       <v-layer>
         <v-group 
-          v-for="(startLetter, messIndex) in splitList(initialMessage)"
-          :key="'MessageLoop'+messIndex"
+          v-for="(letters, messIndex) in splitList"
+          :key="`MessageLoop-${messIndex}`"
           :width="26 * 50"
           :height="50 * messageLen"
           :x="centerShift"
           :y="2 + messIndex * 50"
         >
           <v-group
-            v-for="(letter, index) in shiftArray(((startLetter.charCodeAt(0) - 65) + shift) % 26)"
+            v-for="(letter, index) in letters"
             :key="`InitArray-${letter}-${shift}`"
             :width="50"
             :height="50"
-            :x="index*50"
+            :x="((index + shift) % 26) * 50"
             :y="2"
           >
             <v-rect
@@ -24,15 +24,15 @@
               :height="50"
               stroke="black"
               :stroke-width="2"
-              fill="lightblue"
+              fill="lightgreen"
             />
             <v-rect
-              v-if="index === shift"
+              v-else-if="(index + shift) % 26 === 0"
               :width="50"
               :height="50"
               stroke="black"
               :stroke-width="2"
-              fill="lightgreen"
+              fill="lightblue"
             />
             <v-rect
               v-else
@@ -42,12 +42,12 @@
               :stroke-width="2"
             />
             <v-text
-              :text="index + letter + shift"
+              :text="letter"
               fill="black"
               align="center"
               :width="50"
-              :height="50"
-              :fontSize="14"
+              :height="54"
+              :fontSize="30"
               verticalAlign="middle"
             />
           </v-group>
@@ -77,34 +77,38 @@ export default {
       return this.messageLen * 50 + 20;
     },
     messageLen() {
-      return this.initialMessage.length;
+      return this.sanitizedMessage.length;
     },
     configKonva() {
       return {
         width: window.innerWidth,
         height: this.windowHeight,
       };
-    }
-  },
-  methods: {
-    shiftArray(shift) {
-      let shiftedArray = [];
-      for (let i = 0; i < 26; i++) {
-        shiftedArray.push(String.fromCharCode(((i + shift) % 26) + 65));
-      }
-      return shiftedArray;
     },
-    splitList(initMsg) {
+    sanitizedMessage() {
+      const initMsg = this.initialMessage;
       let newString = '';
-      for(let i = 0; i < initMsg.length; i++){
+      for (let i = 0; i < initMsg.length; i++){
         let curr = initMsg.charCodeAt(i);
         if((curr >= 65 && curr <= 90) || (curr >= 97 && curr <= 122))
           newString += String.fromCharCode(curr);
       }
-      if(newString.length > 13)
+      if (newString.length > 13)
         newString = newString.substring(0,13);
-      let split = newString.toUpperCase().split('');
-      return split;
+      return newString.toUpperCase();
+    },
+    splitList() {
+      return this.sanitizedMessage.split('').map(letter => this.shiftArray(letter));
+    },
+  },
+  methods: {
+    shiftArray(firstLetter) {
+      let shiftedArray = [];
+      const shift = firstLetter.charCodeAt(0) - 65;
+      for (let i = 0; i < 26; i++) {
+        shiftedArray.push(String.fromCharCode((i + shift) % 26 + 65));
+      }
+      return shiftedArray;
     },
   },
 };
