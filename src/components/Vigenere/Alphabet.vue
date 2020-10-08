b9ebea8e4fe691d9432fa5c62f9667ffc2f1d0a1
elafrenz <67011584+elafrenz@users.noreply.github.com>
2020-10-07 22:53:52 -0400

    Alphabet.vue update
    Displaying letters & corresponding numbers using arrays. 
    
    For some reason, having real difficulty displaying all letters and all numbers each on one horizontal line. (currently just one long list)

--- src/components/Vigenere/Alphabet.vue
+++ src/components/Vigenere/Alphabet.vue
@@ -1,19 +1,57 @@
 <template>
   <div>
-    <span>Alphabet list goes here</span>
-    <CircleLetter letter="A" />
+    <span>
+        <span v-for="item in createLetterArr()" v-bind:key="item" display:inline-block>
+          <CircleLetter :letter="item"></CircleLetter>
+        </span>
+        
+        <div v-for="item in createNumArr()" v-bind:key="item" display:inline-block>
+          {{item}}
+        </div>
+    </span>
   </div>
 </template>
 
+
+
 <script>
 import CircleLetter from '../Common/CircleLetter.vue';
-
 export default {
   name: 'VigenereAlphabet',
   components: {
     CircleLetter,
   },
+  methods: {
+    createLetterArr() {
+      let letters = [];
+      for (let i = 0; i < 26; i++) {
+        letters.push(String.fromCharCode(i + 65));
+      }
+      return letters;
+    },
+    createNumArr() {
+      let nums = [];
+      for(let i = 0; i < 26; i++)
+      {
+        nums.push(i);
+      }
+      return nums;
+    }
+  }
 }
+
+//let letterArr = new Vue({
+
+
+//let letterArr = ['a','b','c','d'];
+/*for(let i = 0; i < 26; i++)
+{
+    letterArr.push(String.fromCharCode(i + 97));
+    let letter = String.fromCharCode(i+97);
+    console.log(letter);
+   // document.getElementById('letter').innerHTML = letter;
+}*/
+//console.log(letterArr);
 </script>
 
 <style scoped>
