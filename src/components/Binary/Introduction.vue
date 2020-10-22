078f6d82890db4a83449845ccfa80ad2c37833c5
Link Li <link@langchao.org>
2020-10-22 15:26:23 -0400

    Initialize the binary page

---
+++ src/components/Binary/Introduction.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>XOR Cipher</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BinaryIntro',
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Binary.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Introduction />
+    <Conclusion />
+  </div>
+</template>
+
+<script>
+import Introduction from '../components/Binary/Introduction.vue';
+import Conclusion from '../components/Binary/Conclusion.vue';
+
+export default {
+  name: 'Binary',
+  components: {
+    Introduction,
+    Conclusion,
+  },
+}
+</script>
+
+<style scoped>
+</style>
